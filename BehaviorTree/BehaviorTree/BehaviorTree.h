d347b7cd7c41f311953ead1dcd6542028da4bfc4
Xanhos <grallanyann84@gmail.com>
2024-04-15 18:05:46 +0200

    Push

--- BehaviorTree/BehaviorTree/BehaviorTree.h
+++ BehaviorTree/BehaviorTree/BehaviorTree.h
@@ -4,134 +4,201 @@
 #include <memory>
 #include <functional>
 
-class Node
-{
-public:
-	virtual bool tick() = 0;
-	template<typename T>
-	static std::shared_ptr<T> New(T node) { return std::make_shared<T>(node); }
-};
 
-typedef std::shared_ptr<Node> NodePtr;
-typedef std::list<std::shared_ptr<Node>> NodeList;
+namespace bt
+{
+	class Node
+	{
+		inline static unsigned int m_idCounter = 0;
+	protected:
+		unsigned int m_id;
+		Node() : m_id(m_idCounter++) {}
+
+		template<typename T>
+		std::shared_ptr<T> getNode() { std::make_shared<T>(*this); }
+	public:
+		virtual bool tick() = 0;
+
+		unsigned int getID() const { return m_id; }
+
+		template<typename T>
+		static std::shared_ptr<T> New(const T& node) { return std::make_shared<T>(node); }
+	};
+
+	typedef std::shared_ptr<Node> NodePtr;
+	typedef std::list<std::shared_ptr<Node>> NodeList;
 
 #pragma region COMPOSITE
-
-
-class CompositeNode : public Node
-{
-protected:
-	NodeList m_childList;
-public:
-	NodeList& getChilds() { return m_childList; }
-	template <typename T>
-	std::shared_ptr<T> addChild(std::shared_ptr<T> child) { m_childList.push_back(child); return child; }
-};
-
-class Selector : public CompositeNode
-{
-public:
-	virtual bool tick() {
-		for (auto& child : m_childList)
-		{
-			if (child->tick())
-				return true;
-		}
-		return false;
-	}
-
-};
-
-class Sequence : public CompositeNode
-{
-public:
-	virtual bool tick()
+	namespace Composite
 	{
-		for (auto& child : m_childList)
+		class CompositeNode : public Node
 		{
-			if (!child->tick())
-				return false;
-		}
-		return true;
-	}
-};
+		protected:
+			NodeList m_childList;
+		public:
+			CompositeNode() : Node() {}
 
+			NodeList& getChilds() { return m_childList; }
+
+			template <typename T>
+			std::shared_ptr<T> addChild(std::shared_ptr<T> child) { m_childList.push_back(child); return std::dynamic_pointer_cast<T>(m_childList.back()); }
+
+
+			template <typename T>
+			std::shared_ptr<Node>* addChild_INTERNAL_USE(std::shared_ptr<T> child) { m_childList.push_back(child); return &m_childList.back(); }
+		};
+
+		class Selector : public CompositeNode
+		{
+		public:
+			Selector() : CompositeNode() {}
+			virtual bool tick() {
+				for (auto& child : m_childList)
+				{
+					if (child->tick())
+						return true;
+				}
+				return false;
+			}
+
+		};
+
+		class Sequence : public CompositeNode
+		{
+		public:
+			Sequence() : CompositeNode() {}
+			virtual bool tick()
+			{
+				for (auto& child : m_childList)
+				{
+					if (!child->tick())
+						return false;
+				}
+				return true;
+			}
+		};
+
+		class Alternate : public CompositeNode
+		{
+		public:
+			Alternate() : CompositeNode() {}
+			virtual bool tick()
+			{
+				auto it = m_childList.begin();
+				for (unsigned int index = 0; index < m_childList.size(); index++)
+				{
+					if (index % 2 == 0)
+					{
+						if (!(*it)->tick())
+							return false;
+					}
+					else if ((*it)->tick())
+						return false;
+					it = std::next(it);
+				}
+				return true;
+			}
+		};
+	}
+	
 
 #pragma endregion
 
 #pragma region DECORATOR
 
-
-
-class Decorator : public Node
-{
-protected:
-	NodePtr m_task;
-public:
-	Decorator(NodePtr task) { m_task = task; }
-};
-
-class Inverser : public Decorator
-{
-public:
-	Inverser(NodePtr task) : Decorator(task) {}
-	virtual bool tick() { return !m_task->tick(); }
-};
-
-class Loop : public Decorator
-{
-	int m_loopNbr;
-public:
-	Loop(NodePtr task, int loop) : Decorator(task) { m_loopNbr = loop; }
-	virtual bool tick() {
-		if (m_loopNbr)
-			for (int i = 0; i < m_loopNbr; i++)
-			{
-				if (m_task->tick())
-					return true;
-			}
-		else while (!m_task->tick()) {};
-		
-		return false;
-	}
-};
-
-class Cooldown : public Decorator
-{
-	float m_executionTimer;
-	float m_timer;
-public:
-	Cooldown(NodePtr task, float timer) : Decorator(task) { m_timer = 0.f; m_executionTimer = timer; }
-	virtual bool tick() {
-		if (m_timer > m_executionTimer)
+	namespace Decorator
+	{
+		class Decorator : public Node
 		{
-			m_timer = 0.f;
-			return m_task->tick();
-		}
-		return false;
-	}
-};
+		protected:
+			NodePtr m_task;
+		public:
+			Decorator() { m_task = NodePtr(); }
+			Decorator(NodePtr task) { m_task = task; }
 
-class Condition : public Decorator
-{
-	std::function<bool()> m_condition;
-public:
-	Condition(NodePtr task, std::function<bool()> condition) : Decorator(task) { m_condition = condition; }
-	virtual bool tick() {
-		if (m_condition)
-			if (m_condition())
-				return m_task->tick();
-		return false;
+			NodePtr setTask(NodePtr task) { m_task = task; return m_task; }
+			NodePtr* setTask_INTERNAL_USE(NodePtr task) { m_task = task; return &m_task; }
+		};
+
+		class Inverser : public Decorator
+		{
+		public:
+			Inverser() : Decorator() {}
+			Inverser(NodePtr task) : Decorator(task) {}
+			virtual bool tick() { return !m_task->tick(); }
+		};
+
+		class Loop : public Decorator
+		{
+			unsigned int m_loopNbr;
+		public:
+			Loop() : Decorator() { m_loopNbr = 1u; }
+			Loop(NodePtr task, unsigned int loop) : Decorator(task) { m_loopNbr = loop; }
+
+			void setLoop(unsigned int loop) { m_loopNbr = loop; }
+
+			virtual bool tick() {
+				if (m_loopNbr)
+					for (unsigned int i = 0; i < m_loopNbr; i++)
+					{
+						if (m_task->tick())
+							return true;
+					}
+				else while (!m_task->tick()) {};
+
+				return false;
+			}
+		};
+
+		class Cooldown : public Decorator
+		{
+			float m_executionTimer;
+			float m_timer;
+		public:
+			Cooldown() : Decorator() { m_timer = 0.f; }
+			Cooldown(NodePtr task, float timer) : Decorator(task) { m_timer = 0.f; m_executionTimer = timer; }
+
+			void setTimer(float timer) { m_executionTimer = timer; }
+			virtual bool tick() {
+				if (m_timer > m_executionTimer)
+				{
+					m_timer = 0.f;
+					return m_task->tick();
+				}
+				return false;
+			}
+		};
+
+		class Condition : public Decorator
+		{
+			std::function<bool()> m_condition;
+		public:
+			Condition() : Decorator() {}
+			Condition(NodePtr task, std::function<bool()> condition) : Decorator(task) { m_condition = condition; }
+
+			virtual bool tick() {
+				if (m_condition)
+					if (m_condition())
+						return m_task->tick();
+				return false;
+			}
+		};
+
 	}
-};
 
 #pragma endregion
+	
+	namespace ActionNode
+	{
+		class NodeFunc : public Node
+		{
+			std::function<bool()> m_tick;
+		public:
+			NodeFunc(std::function<bool()> tick) { m_tick = tick; }
+			virtual bool tick() { return m_tick(); }
+		};
+	}
 
+	
+}
 
-class NodeFunc : public Node
-{
-	std::function<bool()> m_tick;
-public:
-	NodeFunc(std::function<bool()> tick) { m_tick = tick; }
-	virtual bool tick() { return m_tick(); }
-};
--- BehaviorTree/BehaviorTree/Source.cpp
+++ BehaviorTree/BehaviorTree/Source.cpp
@@ -1,62 +1,169 @@
 #include "BehaviorTree.h"
 
+using namespace bt;
 
+enum NODETYPE { NONE = -1, SEQUENCE, SELECTOR, ALTERNATE, INVERSER, CONDITION, LOOP, COOLDOWN, OPEN_FRIDGE, FORCE_FRIDGE, TAKE_BEER, CLOSE_FRIDGE };
 
-struct Fridge
+class PatronNode
 {
-	int beer = 10;
-	bool isOpen = false;
+	NODETYPE m_type;
+	std::list<PatronNode> m_child;
+	PatronNode* m_parent;
+public:
+	inline static std::unordered_map<NODETYPE, std::string> m_idToString;
+
+	PatronNode(NODETYPE type) : m_type(type) {};
+
+	PatronNode& add(const PatronNode& node) {
+		if (node.m_type != NONE and this->m_type <= COOLDOWN)
+		{
+			m_child.push_back(node);
+			m_child.back().m_parent = this;
+			return m_child.back();
+		}
+		else
+		{
+			std::cout << "Invalid Node Type" << std::endl;
+			return *this;
+		}
+	}
+
+	NODETYPE getType() { return m_type; }
+	std::list<PatronNode> getChildren() { return m_child; }
+	PatronNode* getParent() { return m_parent; }
+
+	void display(unsigned int space) {
+		for (int i = 0; i < space; i++) std::cout << " ";
+			std::cout << m_idToString[m_type] << std::endl;
+			for (auto& child : m_child) child.display(space + 2);
+	}
 };
+
+
+NodePtr NodeFactory(NODETYPE type)
+{
+	switch (type)
+	{
+	case NONE:
+		break;
+	case SEQUENCE:
+		return Node::New(bt::Composite::Sequence());
+		break;
+	case SELECTOR:
+		return Node::New(bt::Composite::Selector());;
+		break;
+	case ALTERNATE:
+		return Node::New(bt::Composite::Alternate());
+		break;
+	case INVERSER:
+		return Node::New(bt::Decorator::Inverser());
+		break;
+	case CONDITION:
+		return Node::New(bt::Decorator::Condition());
+		break;
+	case LOOP:
+		return Node::New(bt::Decorator::Loop());
+		break;
+	case COOLDOWN:
+		return Node::New(bt::Decorator::Cooldown());
+		break;
+	case OPEN_FRIDGE:
+		return Node::New(bt::ActionNode::NodeFunc([]() {std::cout << "Open Fridge" << std::endl; return true; }));
+		break;
+	case FORCE_FRIDGE:
+		return Node::New(bt::ActionNode::NodeFunc([]() {std::cout << "Force Fridge" << std::endl; return true; }));
+		break;
+	case TAKE_BEER:
+		return Node::New(bt::ActionNode::NodeFunc([]() {std::cout << "Take Beer" << std::endl; return true; }));
+		break;
+	case CLOSE_FRIDGE:
+		return Node::New(bt::ActionNode::NodeFunc([]() {std::cout << "Close Fridge" << std::endl; return true; }));
+		break;
+	default:
+		return Node::New(bt::Composite::Sequence());
+		break;
+	}
+}
+
+void CreateBehaviorTree(NodePtr bt, PatronNode& patron);
+
+NodePtr InitBehavior(PatronNode& patron)
+{
+	NodePtr node;
+
+	node = NodeFactory(patron.getType());
+
+	for (auto& i : patron.getChildren())
+	{
+		CreateBehaviorTree(node, i);
+	}
+
+	return node;
+}
+
+
+void CreateBehaviorTree(NodePtr bt, PatronNode& patron)
+{
+	NodePtr* node = nullptr;
+	if(patron.getParent() == nullptr)
+		*node = bt;
+	else
+	{
+		if (patron.getParent()->getType() < 3)
+			node = std::dynamic_pointer_cast<bt::Composite::CompositeNode>(bt)->addChild_INTERNAL_USE(Node::New(bt::Composite::Selector()));
+		else if (patron.getParent()->getType() < 7)
+			node = std::dynamic_pointer_cast<bt::Decorator::Decorator>(bt)->setTask_INTERNAL_USE(Node::New(bt::Composite::Sequence()));
+		else 
+		{
+			std::cerr << "Invalid Node Type" << std::endl;
+			return;
+		}
+	}
+
+	*node = NodeFactory(patron.getType());
+
+	for (auto& i : patron.getChildren())
+	{
+		CreateBehaviorTree(*node, i);
+	}
+}
+
 
 
 int main()
 {
-	Fridge f;
-	auto root = Node::New(Sequence());
-	//auto openFridge = root->addChild(Node::New(Selector()));
-	//openFridge->addChild(Node::New(FridgeIsOpen(&f)));
-	//openFridge->addChild(Node::New(OpenFridge(&f)));
-	//auto takeABeer = root->addChild(Node::New(Selector()));
-	//takeABeer->addChild(Node::New(TakeABeer(&f)));
-	//takeABeer->addChild(Node::New(CloseFridge(&f)));
-	//root->addChild(Node::New(CloseFridge(&f)));
+	PatronNode::m_idToString[NONE] = "NONE";
+	PatronNode::m_idToString[SEQUENCE] = "SEQUENCE";
+	PatronNode::m_idToString[SELECTOR] = "SELECTOR";
+	PatronNode::m_idToString[ALTERNATE] = "ALTERNATE";
+	PatronNode::m_idToString[INVERSER] = "INVERSER";
+	PatronNode::m_idToString[CONDITION] = "CONDITION";
+	PatronNode::m_idToString[LOOP] = "LOOP";
+	PatronNode::m_idToString[COOLDOWN] = "COOLDOWN";
+	PatronNode::m_idToString[OPEN_FRIDGE] = "OPEN_FRIDGE";
+	PatronNode::m_idToString[FORCE_FRIDGE] = "FORCE_FRIDGE";
+	PatronNode::m_idToString[TAKE_BEER] = "TAKE_BEER";
+	PatronNode::m_idToString[CLOSE_FRIDGE] = "CLOSE_FRIDGE";
+		
 
-	auto openFridge = root->addChild(Node::New(Selector()));
+	PatronNode root(SEQUENCE);
+	auto &Selector_Open = root.add(PatronNode(SELECTOR));
+	Selector_Open.add(PatronNode(OPEN_FRIDGE));
+	Selector_Open.add(PatronNode(FORCE_FRIDGE));
 
-	openFridge->addChild(Node::New(Inverser(Node::New(NodeFunc([&f] {
-		if (f.isOpen)
-			return true;
-		return false;
-		})))));
-	openFridge->addChild(Node::New(NodeFunc([&f] {
-		f.isOpen = true;
-		return true;
-		})));
-	auto takeBeer = root->addChild(Node::New(Selector()));
-	takeBeer->addChild(Node::New(NodeFunc([&f] {
-		if (f.beer > 0)
-		{
-			f.beer--;
-			return true;
-		}
-		return false;
-		})));
-	takeBeer->addChild(Node::New(NodeFunc([&f] {
-		f.isOpen = false;
-		return true;
-		})));
-	root->addChild(Node::New(NodeFunc([&f] {
-		if(f.isOpen)
-		{
-			f.isOpen = false;
-			return true;
-		}
-		return false;
-		})));
+	auto& Selector_Beer = root.add(PatronNode(SELECTOR));
+	auto& Condition_Beer = Selector_Beer.add(PatronNode(CONDITION));
+	Condition_Beer.add(PatronNode(TAKE_BEER));
+	Selector_Beer.add(PatronNode(CLOSE_FRIDGE));
+	root.add(PatronNode(CLOSE_FRIDGE));
 
 
-	while (root->tick())
-		std::cout << "Etat du frigo : " << (!f.isOpen ? "Fermer" : "Ouvert") << "\nNombre de bière : " << f.beer << "\n";
+	auto root_bt = InitBehavior(root);
 
-	std::cout << "Etat du frigo : " << (!f.isOpen ? "Fermer" : "Ouvert") << "\nNombre de bière : " << f.beer << "\n";
+
+	root_bt->tick();
+
+
+
+	root.display(0);
 }
